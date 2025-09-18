184df3937eac61fb5c2d000446b96829688f460a
safiye-dalkiran <safiyedal360@gmail.com>
2025-09-18 18:08:43 +0300

    Proje setup’ı tamamlandı

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import Login from "./components/Login";
+import Success from "./components/Success";
+function App() {
+
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
